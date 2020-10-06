4dcd04801a73a2a2cef1c82b66086fbe3e0e7f23
Heather1337 <heatherlynn1337@gmail.com>
2020-10-05 19:58:36 -0700

    Updating melon print function to printing all melons and attributes with updated attributes.

--- accounting-scripts/melon_info.py
+++ accounting-scripts/melon_info.py
@@ -1,18 +1,13 @@
 """Print out all the melons in our inventory."""
+from melons import melons
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
-
-
-def print_melon(name, seedless, price):
+def print_melons(melons):
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+    for melon, attribute in melons.items():
+        print('MELON TYPE:', melon)
+        for attribute, value in attribute.items():
+            print(attribute, value) 
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
-
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+print_melons(melons)
--- accounting-scripts/melons.py
+++ accounting-scripts/melons.py
@@ -1,23 +1,61 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
+melons = {
+    'Honeydew': {
+        'price': 0.99,
+        'seedlessness': True ,
+        'flesh_color': None,
+        'rind_color': None,
+        'avg_weight': None,
+        'local': False
+    },
+    'Crenshaw': {
+        'price': 2.00,
+        'seedlessness': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'avg_weight': None,
+        'local': False
+    },
+    'Crane': {
+        'price': 2.50,
+        'seedlessness': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'avg_weight': None,
+        'local': False
+    },
+    'Casaba': {
+        'price': 2.50,
+        'seedlessness': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'avg_weight': None,
+        'local': False
+    },
+    'Cantaloupe': {
+        'price': 0.99,
+        'seedlessness': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'avg_weight': None,
+        'local': False
+    }
 }
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
+# print(melon_names['cantaloupe'])
 
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
+
+# melon_prices = {
+#     1: 0.99,
+#     2: 2.00,
+#     3: 2.50,
+#     4: 2.50,
+#     5: 0.99,
+# }
+
+# melon_seedlessness = {
+#     1: True,
+#     2: False,
+#     3: False,
+#     4: False,
+#     5: False,
+# }
