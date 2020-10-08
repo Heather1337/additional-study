3bdd7b0bf1586acd22092be959115f6658f93b4b
Heather1337 <heatherlynn1337@gmail.com>
2020-10-07 20:52:48 -0700

    Fixing bug and finishing code in version 2-- raffle file. Separated customer logic from raffle.

--- melon-raffle/version2/raffle.py
+++ melon-raffle/version2/raffle.py
@@ -1,6 +1,18 @@
-"""Randomly pick customer and print customer info"""
+import customers
+from random import choice
 
-# Add code starting here
 
+def run_raffle():
+    """Randomly pick customer and print customer info"""
+
+    customers_list = customers.get_customers_from_file('customers.txt')
+    chosen_customer = choice(customers_list)
+
+    print("Tell {name} at {email} that they've won".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+        ))
+
+run_raffle()   
 # Hint: remember to import any functions you need from
 # other files or libraries
